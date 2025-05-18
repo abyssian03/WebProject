b22c4b894818108f45bba5720089324dcfc85751
abyssian03 <abyssian03@gmail.com>
2025-05-18 18:34:53 +0300

    Реализовано добавление пользователя в фейковую базу

--- lib_prj/app/main.py
+++ lib_prj/app/main.py
@@ -1,9 +1,11 @@
 import logging
 
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, Form
+from fastapi.responses import FileResponse
 
-from app.config import load_config
+from app.models.models import User
+
+#from app.config import load_config
 
 
 app = FastAPI()
@@ -12,27 +14,33 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
+fake_db = [{"username": "vasya", "user_info": "любит колбасу"}, {"username": "katya", "user_info": "любит петь"}]
+
 @app.get("/")
-def read_root():
-    logger.info("Handling request to root endpoint")
-    return {"message": "Hello, World!"}
+async def root():
+    return {"message": "Добро пожаловать!"}
 
+@app.get("/add")
+async def index():
+    return FileResponse("index.html")
 
-class User(BaseModel):
-    username: str
-    message: str
+@app.get('/users')
+async def get_all_users():
+    return fake_db
 
+@app.post("/add")
+async def index(name: str = Form(), info: str = Form()):
+    new_user = {"username": name, "user_info": info}
+    if new_user in fake_db:
+        return {"message": "Такой пользователь уже есть!"}
+    else:
+        your_user = User(**new_user)
+        fake_db.append(your_user)
+        return {"message": "Пользователь добавлен!"}
 
-@app.post("/")
-async def root(user: User):
-    """ """
-    print(f"Мы получили от юзера {user.username} такое сообщение: {user.message}")
-    return user
+#config = load_config()
 
-
-config = load_config()
-
-if config.debug:
-    app.debug = True
-else:
-    app.debug = False
+# if config.debug:
+#     app.debug = True
+# else:
+#     app.debug = False
--- lib_prj/app/models/models.py
+++ lib_prj/app/models/models.py
@@ -4,3 +4,7 @@ from pydantic import BaseModel
 class Product(BaseModel):
     name: str
     price: int
+
+class User(BaseModel):
+    username: str
+    user_info: str
