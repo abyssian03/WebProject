1963dac0695ed45c6477ddeadc79c7a0d98baab7
abyssian03 <abyssian03@gmail.com>
2025-05-19 09:14:47 +0300

    Делаем нумерацию пользователей

--- lib_prj/app/main.py
+++ lib_prj/app/main.py
@@ -3,7 +3,7 @@ import logging
 from fastapi import FastAPI, Form
 from fastapi.responses import FileResponse
 
-from app.models.models import User
+from app.models.models import User, Feedback
 
 #from app.config import load_config
 
@@ -14,16 +14,19 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-fake_db = [{"username": "vasya", "user_info": "любит колбасу"}, {"username": "katya", "user_info": "любит петь"}]
+fake_db = {1: {"username": "vasya", "user_info": "любит колбасу"}, 2: {"username": "katya", "user_info": "любит петь"}}
 
 
 
 # Получение списка пользователей с параметрами
 @app.get('/users')
 async def get_users(username: str = '', limit: int = 10):
-    filtered_users = [user for user in fake_db if username.lower() in user["username"].lower()]
+    filtered_users = fake_users
 
-    return filtered_users[:limit]
+    # if username:
+    #     filtered_users = {key: user for key, user in filtered_users.items() if username.lower() in user["username"].lower()}
+
+    return dict(list(filtered_users.items())[:limit])
 
 
 #Вывод формы для добавления пользователя
@@ -36,13 +39,21 @@ async def add_user():
 @app.post("/add")
 async def add_user(name: str = Form(), info: str = Form()):
     new_user = {"username": name, "user_info": info}
-    if new_user in fake_db:
-        return {"message": "Такой пользователь уже есть!"}
-    else:
-        your_user = User(**new_user)
-        fake_db.append(your_user)
-        return {"message": "Пользователь добавлен!"}
+    for key in fake_db:
+        if new_user == fake_db[key]:
+            return {"message": "Такой пользователь уже есть!"}
+        else:
+            your_user = User(**new_user)
+            fake_db.update({len(fake_db)+1: your_user})
+            return {"message": "Пользователь добавлен!"}
 
+#Получение фидбэка
+# @app.post("/feedback")
+# async def get_feedback(data: Feedback):
+#     for user in fake_db:
+#         if data.username == user["username"]:
+#             return {"message": f"Спасибо за отзыв, {data.username}!"}
+#     return {"message": f"Пользователь {data.username} не зарегистрирован!"}
 
 #Вывод сообщения о входе c параметром
 @app.get("/{username}")
